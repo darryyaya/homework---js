847a2d89a814cd08bb71995475d4906f4d36b18c
Дударева Дарья Леонидовна <29474055@s.mfui.ru>
2026-02-03 11:57:44 +0300

    meow

--- hv33.js
+++ hv33.js
@@ -1,16 +1,15 @@
-  
-    let a = +prompt();
-    switch(a){
-        case 3:
-        alert("мало");
-        break;
-        case 4:
-        alert("в точку!");
-        break;
-        case 5:
-        alert("много!");
-        break;
-    default:
-        alert("Мимо!!!");
-        break;
+let n = +prompt("Vvedite vashe chislo: ")
+if (n % 2 === 0){
+    let sum = 1;
+    for(let i = 0; i < n; i++){
+        sum = sum + i;
+        console.log(sum);
     }
+}
+else{
+    let mult = 1;
+    for(let i = 1; i < n; i++){
+        mult = mult * i;
+        console.log(mult);
+    }
+}
